fff15e95969f916fb0594b920d09e03862c4d17c
Dio Prasetyo <prasetyodio04@gmail.com>
2026-02-14 13:06:38 +0700

    Update Fitur Airport

---
+++ app/Filament/Resources/AirportResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AirportResource\Pages;
+use App\Filament\Resources\AirportResource\RelationManagers;
+use App\Models\Airport;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class AirportResource extends Resource
+{
+    protected static ?string $model = Airport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                FileUpload::make('image')->image()->directory('airports')->required()->columnSpan(2),
+                TextInput::make('iata_code')->required(),
+                TextInput::make('name')->required(),
+                TextInput::make('city')->required(),
+                TextInput::make('country')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image')->size(60)->circular()->label('Image'),
+                TextColumn::make('iata_code')->label('IATA Code'),
+                TextColumn::make('name')->label('Name'),
+                TextColumn::make('city')->label('City'),
+                TextColumn::make('country')->label('Country'),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                ViewAction::make(),
+                DeleteAction::make(),
+                RestoreAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAirports::route('/'),
+            'create' => Pages\CreateAirport::route('/create'),
+            'edit' => Pages\EditAirport::route('/{record}/edit'),
+        ];
+    }
+}
