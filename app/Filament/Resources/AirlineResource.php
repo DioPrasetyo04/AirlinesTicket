4805fbd699d1d5c480ed5a58e59239f1d992ddf2
Dio Prasetyo <prasetyodio04@gmail.com>
2026-02-14 13:18:50 +0700

    Update CMS Airline

--- app/Filament/Resources/AirlineResource.php
+++ app/Filament/Resources/AirlineResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\AirportResource\Pages;
-use App\Filament\Resources\AirportResource\RelationManagers;
-use App\Models\Airport;
+use App\Filament\Resources\AirlineResource\Pages;
+use App\Filament\Resources\AirlineResource\RelationManagers;
+use App\Models\Airline;
 use Filament\Forms;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
@@ -13,16 +13,20 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\ForceDeleteAction;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
 use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\RestoreBulkAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class AirportResource extends Resource
+class AirlineResource extends Resource
 {
-    protected static ?string $model = Airport::class;
+    protected static ?string $model = Airline::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
@@ -30,11 +34,9 @@ class AirportResource extends Resource
     {
         return $form
             ->schema([
-                FileUpload::make('image')->image()->directory('airports')->required()->columnSpan(2),
-                TextInput::make('iata_code')->required(),
+                FileUpload::make('logo')->image()->directory('airlines')->required()->columnSpan(2),
+                TextInput::make('code')->required(),
                 TextInput::make('name')->required(),
-                TextInput::make('city')->required(),
-                TextInput::make('country')->required(),
             ]);
     }
 
@@ -42,11 +44,9 @@ class AirportResource extends Resource
     {
         return $table
             ->columns([
-                ImageColumn::make('image')->size(60)->circular()->label('Image'),
-                TextColumn::make('iata_code')->label('IATA Code'),
+                ImageColumn::make('logo')->size(60)->circular()->label('Image'),
+                TextColumn::make('code')->label('Code'),
                 TextColumn::make('name')->label('Name'),
-                TextColumn::make('city')->label('City'),
-                TextColumn::make('country')->label('Country'),
             ])
             ->filters([
                 TrashedFilter::make(),
@@ -61,6 +61,8 @@ class AirportResource extends Resource
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
                 ]),
             ]);
     }
@@ -75,9 +77,9 @@ class AirportResource extends Resource
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListAirports::route('/'),
-            'create' => Pages\CreateAirport::route('/create'),
-            'edit' => Pages\EditAirport::route('/{record}/edit'),
+            'index' => Pages\ListAirlines::route('/'),
+            'create' => Pages\CreateAirline::route('/create'),
+            'edit' => Pages\EditAirline::route('/{record}/edit'),
         ];
     }
 }
